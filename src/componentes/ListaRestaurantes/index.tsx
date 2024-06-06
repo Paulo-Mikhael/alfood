9034de6a7f721cc18e1064fed2b0366216d78418
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2024-06-06 09:54:24 -0400

    Input de pesquisa por restaurantes

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -3,14 +3,15 @@ import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 import axios from 'axios';
-import { IPaginacao } from '../../interfaces/IPaginacao';
-import { Button } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 
 const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
   const [restaurantesAmostra, setRestaurantesAmostra] = useState<IRestaurante[]>([]);
-  const [qtdParaMostrar, setQtdParaMostrar] = useState<number>(10);
+  const qtdPraMostrarInicial = 3;
+  const [qtdParaMostrar, setQtdParaMostrar] = useState<number>(qtdPraMostrarInicial);
+  const [filtrando, setFiltrando] = useState<boolean>(false);
 
   useEffect(() => {
     axios.get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
@@ -27,22 +28,38 @@ const ListaRestaurantes = () => {
   const mostraDezRestaurantes = () => {
     if (restaurantes.length > qtdParaMostrar){
       const dezRestaurantes = [];
-      for (let i = qtdParaMostrar - 10; i < qtdParaMostrar; i++){
+      for (let i = qtdParaMostrar - qtdPraMostrarInicial; i < qtdParaMostrar; i++){
         restaurantes[i] && dezRestaurantes.push(restaurantes[i]);
       }
 
-      qtdParaMostrar == 10 ? setRestaurantesAmostra(dezRestaurantes) : setRestaurantesAmostra([...restaurantesAmostra, ...dezRestaurantes]);
+      qtdParaMostrar == qtdPraMostrarInicial ? setRestaurantesAmostra(dezRestaurantes) : setRestaurantesAmostra([...restaurantesAmostra, ...dezRestaurantes]);
       console.log(restaurantesAmostra);
-      setQtdParaMostrar(qtdParaMostrar + 10);
+      setQtdParaMostrar(qtdParaMostrar + qtdPraMostrarInicial);
     }else{
       setRestaurantesAmostra(restaurantes);
     }
   }
+  const pesquisaRestaurante = (pesquisa: string) => {
+    setFiltrando(true);
+
+    const filter = restaurantes.filter(restaurante => restaurante.nome.includes(pesquisa));
+
+    setRestaurantesAmostra(filter);
+  }
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    <div className={style.PesquisaContainer}>
+      <TextField 
+        fullWidth 
+        id="outlined-basic" 
+        label="Pesquise um restaurante!" 
+        variant="outlined" 
+        onChange={(evt) => pesquisaRestaurante(evt.target.value)}
+      />
+    </div>
     {restaurantesAmostra.map(item => <Restaurante restaurante={item} key={item.id} />)}
-    {restaurantesAmostra.length != restaurantes.length && restaurantesAmostra.length > 0 && 
+    {restaurantesAmostra.length != restaurantes.length && restaurantesAmostra.length > 0 && filtrando === false &&
     <Button variant="outlined" onClick={mostraDezRestaurantes}>
       Ver mais
     </Button>}
