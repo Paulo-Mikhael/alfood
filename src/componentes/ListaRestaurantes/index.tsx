afcdb5984e455e0edd4db0ce43c248503755f605
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2024-06-07 09:40:23 -0400

    Carregando pratos na vitrine

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,70 +1,44 @@
+import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { IPaginacao } from '../../interfaces/IPaginacao';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
-import axios from 'axios';
-import { Button, TextField } from '@mui/material';
-import { IPaginacao } from '../../interfaces/IPaginacao';
 
 const ListaRestaurantes = () => {
 
-  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
-  const [restaurantesAmostra, setRestaurantesAmostra] = useState<IRestaurante[]>([]);
-  const qtdPraMostrarInicial = 3;
-  const [qtdParaMostrar, setQtdParaMostrar] = useState<number>(qtdPraMostrarInicial);
-  const [filtrando, setFiltrando] = useState<boolean>(false);
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+  const [proximaPagina, setProximaPagina] = useState('')
 
   useEffect(() => {
-    axios.get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
+    // obter restaurantes
+    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
       .then(resposta => {
-        setRestaurantes(resposta.data);
+        setRestaurantes(resposta.data.results)
+        setProximaPagina(resposta.data.next)
       })
-      .catch(error => {
-        console.log(error);
-      });
+      .catch(erro => {
+        console.log(erro)
+      })
   }, [])
-  useEffect(() => {
-    mostraDezRestaurantes();
-  }, [restaurantes]);
-  const mostraDezRestaurantes = () => {
-    if (restaurantes.length > qtdParaMostrar){
-      const dezRestaurantes = [];
-      for (let i = qtdParaMostrar - qtdPraMostrarInicial; i < qtdParaMostrar; i++){
-        restaurantes[i] && dezRestaurantes.push(restaurantes[i]);
-      }
 
-      qtdParaMostrar == qtdPraMostrarInicial ? setRestaurantesAmostra(dezRestaurantes) : setRestaurantesAmostra([...restaurantesAmostra, ...dezRestaurantes]);
-      console.log(restaurantesAmostra);
-      setQtdParaMostrar(qtdParaMostrar + qtdPraMostrarInicial);
-    }else{
-      setRestaurantesAmostra(restaurantes);
-    }
-  }
-  const pesquisaRestaurante = (pesquisa: string) => {
-    setFiltrando(true);
-
-    const filter = restaurantes.filter(restaurante => restaurante.nome.toLowerCase().includes(pesquisa.toLowerCase()));
-
-    setRestaurantesAmostra(filter);
-    console.log(pesquisa);
+  const verMais = () => {
+    axios.get<IPaginacao<IRestaurante>>(proximaPagina)
+      .then(resposta => {
+        setRestaurantes([...restaurantes, ...resposta.data.results])
+        setProximaPagina(resposta.data.next)
+      })
+      .catch(erro => {
+        console.log(erro)
+      })
   }
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
-    <div className={style.PesquisaContainer}>
-      <TextField 
-        fullWidth 
-        id="outlined-basic" 
-        label="Pesquise um restaurante!" 
-        variant="outlined" 
-        onChange={(evt) => pesquisaRestaurante(evt.target.value)}
-      />
-    </div>
-    {restaurantesAmostra.map(item => <Restaurante restaurante={item} key={item.id} />)}
-    {restaurantesAmostra.length != restaurantes.length && restaurantesAmostra.length > 0 && filtrando === false &&
-    <Button variant="outlined" onClick={mostraDezRestaurantes}>
-      Ver mais
-    </Button>}
+    {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+    {proximaPagina && <button onClick={verMais}>
+      ver mais
+    </button>}
   </section>)
 }
 
